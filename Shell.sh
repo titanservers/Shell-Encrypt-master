293b7fe4c9a45765805b972054e144253c42f00d
titanservers <54300844+titanservers@users.noreply.github.com>
2020-01-06 00:32:27 -0500

    Update Shell.sh

--- Shell.sh
+++ Shell.sh
@@ -7,30 +7,30 @@ Welcome='
 ==========================================================================
                                Shell Encrypt 
                                
-                         Powered by 52Fancy 2015-2016                     
+                         Powered by TITAN_ORIGIN                      
                               All Rights Reserved                  
                                                                             
 ==========================================================================
 ';
 echo "$Welcome";
-echo -n "Please Input Script Name :  "
+echo -n "Por favor ingrese el nombre del script :  "
 read Name
 if [ -z $Name ]
 	then
-		echo -n " Script Name is Null "
+		echo -n " El nombre del script es nulo "
 		read Name
 	if [ -z $Name ]
 	    then
-		    echo "Script Name Can't Be Read "
+		    echo "El nombre del script no se puede leer "
 		    exit 0;
 	    fi	
 fi
 if [ ! -e "$Name" ]
     then
-	    echo "Script Name Can't be Found "
+	    echo "El nombre del script no se puede encontrar "
 		exit 0;
 fi
-echo "Please Waiting .........."
+echo "Por favor espera .........."
 yum install -y gcc-c++ gcc automake autoconf libtool make >/dev/null 2>&1
 if [ ! -e "/usr/local/man/man1/" ]
 then
@@ -40,6 +40,7 @@ wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9b.tgz >/dev/null 2>&1
 tar zxvf shc-3.8.9b.tgz >/dev/null 2>&1
 rm -f shc-3.8.9b.tgz
 cd shc-3.8.9b >/dev/null 2>&1
+cp shc-3.8.9b.c shc.c >/dev/null 2>&1
 make install <<EOF
 y
 EOF
